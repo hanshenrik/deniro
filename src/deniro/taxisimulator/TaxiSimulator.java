337b0409230a8ff45af2ab1c9456757f26ba514e
Anders Brastad <abrastad@gmail.com>
2014-05-02 19:31:33 +0200

    Modified TaxiSimulator

--- src/deniro/taxisimulator/TaxiSimulator.java
+++ src/deniro/taxisimulator/TaxiSimulator.java
@@ -15,42 +15,54 @@ public class TaxiSimulator extends Block {
 	private String curPos = "Elgesetergate 1";
 	public java.lang.String taxiAlias;
 	public java.lang.String destination = "Sunnlandsvegen 64";
-	
+	public no.ntnu.item.ttm4115.simulation.routeplanner.Route r;
+	public int routeStepCounter;
+	public int steps;
 	public Journey makeJourney(){
 		Journey j = new Journey(taxiAlias, curPos,destination);
 		return j;
 	}
 
 	public Route printRoute(Route r) {
+		routeStepCounter = 0;
+		steps = r.legs.get(0).steps.size();
 		for (int i = 0; i < r.legs.get(0).steps.size(); i++) {
 			float lat = (float) (r.legs.get(0).steps.get(i).endLocation.latitude*1E6);
 			float lng = (float) (r.legs.get(0).steps.get(i).endLocation.longitude*1E6);
 			System.out.println("p.addPoint(new Position p"+i+1+"("+lat+","+lng+")");
 		}
+//		int i = r.legs.get(0).duration.value;
 		return r;
 	}
 
 	public MapUpdate addPolyline(MapUpdate m){
+		m.setCenter(new Position (6.3422984E7,1.0394329E7));
+		m.setZoom(15);
 		Polyline p1 = new Polyline(taxiAlias);
+		Polyline p2 = new Polyline(taxiAlias+"drive");
 		p1.hue(Marker.HUE_VIOLET);
-		p1.addPoint(new Position (6.3422984E7,1.0394329E7));
-		p1.addPoint(new Position (6.3424928E7,1.0393792E7));
-		p1.addPoint(new Position (6.3427508E7,1.0393112E7));
-		p1.addPoint(new Position (6.3427696E7,1.0396566E7));
-		p1.addPoint(new Position (6.3428928E7,1.0395916E7));
-		p1.addPoint(new Position (6.3429064E7,1.0392618E7));
-		p1.addPoint(new Position (6.3424928E7,1.0393792E7));
-		p1.addPoint(new Position (6.3422984E7,1.0394329E7));
-		p1.addPoint(new Position (6.3414984E7,1.039808E7));
-		p1.addPoint(new Position (6.3408608E7,1.0398607E7));
-		p1.addPoint(new Position (6.3408376E7,1.0401408E7));
-		p1.addPoint(new Position (6.3408696E7,1.0403184E7));
-		p1.addPoint(new Position (6.3407896E7,1.0403862E7));
-		p1.addPoint(new Position (6.340548E7,1.0407723E7));
-		p1.addPoint(new Position (6.3405152E7,1.0409483E7));
-		p1.addPoint(new Position (6.3404756E7,1.0406923E7));
-		p1.addPoint(new Position (6.3404808E7,1.0404997E7));
-		m.addPolyline(p1);
+		p2.hue(Marker.HUE_CYAN);
+		Position endPoint = new Position(0,0);;
+		for (int i = routeStepCounter; i < steps; i++) {
+			float lat = (float) (r.legs.get(0).steps.get(i).endLocation.latitude*1E6);
+			float lng = (float) (r.legs.get(0).steps.get(i).endLocation.longitude*1E6);
+			
+			if(i==0){p1.addPoint(new Position (lat,lng));}
+			if (i+1 == steps) {endPoint = new Position(lat,lng);}
+			
+			p2.addPoint(new Position (lat,lng));
+			
+		}
+		if (routeStepCounter == r.legs.get(0).steps.size()){
+			m.getPolylines().clear();
+			Marker m1 = Marker.createMarker(taxiAlias).position(endPoint).hue(Marker.HUE_GREEN).description("Arrived destination").title(taxiAlias);
+			m.addMarker(m1);
+		}
+		else{
+			m.addPolyline(p1);
+			m.addPolyline(p2);
+		}
+		routeStepCounter++;
 		return m;
 	}
 }
